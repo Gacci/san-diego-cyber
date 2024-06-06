489ec21f843f3f96c1c03b694eb8b7849c2b8569
Jonathan <justo.jonathan@gmail.com>
2024-06-05 20:41:35 -0700

    Deploy Test

---
+++ dist/js/155.8e1dadd2.js
@@ -0,0 +1,8 @@
+"use strict";(self["webpackChunkjs_junkie"]=self["webpackChunkjs_junkie"]||[]).push([[155],{5155:function(n,t,e){e.r(t),e.d(t,{allDay:function(){return y},createEvent:function(){return l},dateFormat:function(){return M},day:function(){return f},default:function(){return Y},deleteEvent:function(){return k},month:function(){return c},months:function(){return u},noEvent:function(){return s},today:function(){return i},week:function(){return d},weekDays:function(){return r},year:function(){return a},years:function(){return o}});
+/**
+  * vue-cal v4.8.1
+  * (c) 2022 Antoni Andre <antoniandre.web@gmail.com>
+  * @license MIT
+  */
+const r=["Понеделник","Вторник","Сряда","Четвъртък","Петък","Събота","Неделя"],u=["Януари","Февруари","Март","Април","Май","Юни","Юли","Август","Септември","Октомври","Ноември","Декември"],o="Години",a="Година",c="Месец",d="Седмица",f="Ден",i="Днес",s="Няма събития",y="Цял ден",k="Изтрий",l="Създай събитие",M="dddd D MMMM YYYY",Y={weekDays:r,months:u,years:"Години",year:"Година",month:"Месец",week:"Седмица",day:"Ден",today:"Днес",noEvent:"Няма събития",allDay:"Цял ден",deleteEvent:"Изтрий",createEvent:"Създай събитие",dateFormat:"dddd D MMMM YYYY"}}}]);
+//# sourceMappingURL=155.8e1dadd2.js.map
